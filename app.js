69a337d74d204499e890d5fe87e0299c699b914e
Darwin_otalvaro <darwinlo126@gmail.com>
2026-07-01 16:58:15 -0500

    creacion del css

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ let ciudades=[
     {nombre: "Cali",numeroHabitantes:5000000,metrosSM:5000,temperatura:"30°",coordenadas:[120,-280],sitios:[{"01":"plaza","02":"piscina"}]},
     {nombre: "Pereira",numeroHabitantes:2000000,metrosSM:10000,temperatura:"20°",coordenadas:[220,-180],sitios:[{"01":"zoologico","02":"parque del cafe"}]},
     {nombre: "florencia",numeroHabitantes:1000000,metrosSM:1000,temperatura:"35°",coordenadas:[20,-80],sitios:[{"01":"museo principal","02":"parque principal"}]},
-    {nombre: "Cartagena",numeroHabitantes:1000000,metrosSM:1000,temperatura:"35°",coordenadas:[10,-50],sitios:[{"01":"playa blanca","02":"ciudad murallada"}]},
-]
+    {nombre: "Cartagena",numeroHabitantes:1000000,metrosSM:1000,temperatura:"35°",coordenadas:[10,-50],sitios:[{"01":"playa blanca","02":"ciudad murallada"}]},]
 let contenidoTabla=document.getElementById('contenidoTabla');
 
 function cargar(arreglo)
@@ -17,7 +16,6 @@ function cargar(arreglo)
     <td>${objeto.temperatura}</td>
     <td>${objeto.coordenadas}</td>
     <td>${Object.values(objeto.sitios[0]).join(' , ')}</td>
-
     </tr>`
 ).join('');
 }
