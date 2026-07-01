2b61b6a5131e7e45b3679ed7125fdf7f2445b335
Darwin_otalvaro <darwinlo126@gmail.com>
2026-07-01 17:05:07 -0500

    agregue tres ciudades mas al objeto

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ let ciudades=[
     {nombre: "Cali",numeroHabitantes:5000000,metrosSM:5000,temperatura:"30°",coordenadas:[120,-280],sitios:[{"01":"plaza","02":"piscina"}]},
     {nombre: "Pereira",numeroHabitantes:2000000,metrosSM:10000,temperatura:"20°",coordenadas:[220,-180],sitios:[{"01":"zoologico","02":"parque del cafe"}]},
     {nombre: "florencia",numeroHabitantes:1000000,metrosSM:1000,temperatura:"35°",coordenadas:[20,-80],sitios:[{"01":"museo principal","02":"parque principal"}]},
-    {nombre: "Cartagena",numeroHabitantes:1000000,metrosSM:1000,temperatura:"35°",coordenadas:[10,-50],sitios:[{"01":"playa blanca","02":"ciudad murallada"}]},]
+    {nombre: "Cartagena",numeroHabitantes:1000000,metrosSM:1000,temperatura:"35°",coordenadas:[10,-50],sitios:[{"01":"playa blanca","02":"ciudad murallada"}]},
+    {nombre: "coveñas",numeroHabitantes:3000000,metrosSM:10000,temperatura:"30°",coordenadas:[220,-180],sitios:[{"01":"zoologico","02":"parque del cafe"}]},
+    {nombre: "santamarta",numeroHabitantes:5000000,metrosSM:1000,temperatura:"33°",coordenadas:[20,-80],sitios:[{"01":"museo principal","02":"parque principal"}]},
+    {nombre: "barranquilla",numeroHabitantes:1000000,metrosSM:1000,temperatura:"32°",coordenadas:[10,-50],sitios:[{"01":"playa blanca","02":"ciudad murallada"}]}]
 let contenidoTabla=document.getElementById('contenidoTabla');
 
 function cargar(arreglo)
